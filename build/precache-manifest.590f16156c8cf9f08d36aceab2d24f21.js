0d63023c47a2be065774056da4d5b8c9392fcc90
Leandro Gonçalves <leandro.goncalves@picpay.com>
2023-11-23 20:38:30 -0300

    Versionando pasta build

---
+++ build/precache-manifest.590f16156c8cf9f08d36aceab2d24f21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e44655ed7a40ea43a962fbc1841476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1761023fd4dbe9d21183",
+    "url": "/static/js/2.051718e4.chunk.js"
+  },
+  {
+    "revision": "097290fd5f9309996cf6c672cbf11328",
+    "url": "/static/js/2.051718e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e98e04f20a15695f9b",
+    "url": "/static/js/main.b9e43591.chunk.js"
+  },
+  {
+    "revision": "ebe5bbabfcfe71404f0c",
+    "url": "/static/js/runtime-main.1454b1ff.js"
+  }
+]);
